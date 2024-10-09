758cc98d62ab47a3d2a719b09ec3dcf58e4e58f5
v2~Nex <nexxonofficial@gmail.com>
2024-10-09 23:25:16 +0100

    push: Attempting to get hovers to work

--- client.lua
+++ client.lua
@@ -2,13 +2,55 @@ exports('actuation', Init)
 
 function Init()
     ShutdownLoadingScreenNui()
-    InitiateSelectionScaleform()
+    local handle = InitiateSelectionScaleform()
+
+    while true do
+        DrawScaleformMovieFullscreen(handle, 255, 255, 255, 255, 0)
+        Wait(0)
+    end
 end
 
 CreateThread(function()
     Init()
 end)
 
+-- Initialising the selector (grid menu) scaleform.
+-- RETURNS: The handle for the scaleform.
 function InitiateSelectionScaleform()
+    local handle = RequestScaleformMovie('MP_NEXT_JOB_SELECTION')
+    while not HasScaleformMovieLoaded(handle) do
+        Wait(0)
+    end 
 
+    BeginScaleformMovieMethod(handle, 'SET_TITLE')
+    PushScaleformMovieMethodParameterString('Select A Gamemode')
+    PushScaleformMovieMethodParameterString('')
+    EndScaleformMovieMethod()
+
+    BeginScaleformMovieMethod(handle, 'SET_GRID_ITEM')
+    ScaleformMovieMethodAddParamInt(0)
+    PushScaleformMovieMethodParameterString('Test')
+    PushScaleformMovieMethodParameterString('') -- Texture Directory
+    PushScaleformMovieMethodParameterString('') -- Texture String
+    ScaleformMovieMethodAddParamInt(0) -- Texture Load Type
+    ScaleformMovieMethodAddParamBool(false) -- Verified Type
+    ScaleformMovieMethodAddParamInt(0) -- Icon?
+    ScaleformMovieMethodAddParamBool(false) -- Check?
+    ScaleformMovieMethodAddParamInt(0) -- RP Multiplier
+    ScaleformMovieMethodAddParamInt(0) -- Cash Multiplier
+    ScaleformMovieMethodAddParamBool(false) -- Disabled
+    ScaleformMovieMethodAddParamInt(0) -- Column
+    ScaleformMovieMethodAddParamInt(0) -- AP Multiplier
+    EndScaleformMovieMethod()
+
+    Wait(100)
+
+    BeginScaleformMovieMethod('SET_SELECTION')
+    ScaleformMovieMethodAddParamInt(0)
+    PushScaleformMovieMethodParameterString('Test')
+    PushScaleformMovieMethodParameterString('Another Test')
+    ScaleformMovieMethodAddParamBool(false)
+    EndScaleformMovieMethod()
+
+    return handle
 end
